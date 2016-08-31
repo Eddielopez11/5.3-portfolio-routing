b9068fcc5afe73d337514a40f285a5bfbf313745
Eduardo Lopez <eddielopez2101@gmail.com>
2016-08-31 16:58:48 -0500

    new files created, and nested/links working

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Main from './lib/main'
+import { Router, Route, hashHistory } from 'react-router'
+import Navbar from './lib/Navbar'
+import Profile from './lib/Profile'
+import Blog from './lib/Blog'
+import Projects from './lib/Projects'
+import ProjectDetails from './lib/ProjectDetails'
 
 render((
-  <Main name="Boilerplate"/>
-), document.getElementById('app'))
+  <Router history={ hashHistory }>
+    <Route path="/" component={Navbar}>
+      <Route path="/profile" component={ Profile }/>
+      <Route path="/blog" component={ Blog }/>
+      <Route path="/projects" component={ Projects }/>
+      <Route path="/projectdetails" component={ ProjectDetails }/>
+    </Route>
+  </Router>
+), document.getElementById('portfolio'))
